01b586312f078c4219540bb39451b7c24a84ceef
Tushar Shiralkar <tushardinesh.shiralkar@analog.com>
2024-02-20 16:00:24 +0100

    Declared functions in TMC5271_Simple_Rotation.h

---
+++ TMC5271-EVAL/TMC5271_TMC_API/TMC5271_Simple_Rotation.h
@@ -0,0 +1,13 @@
+/*******************************************************************************
+* Copyright © 2023 Analog Devices Inc. All Rights Reserved. This software is
+* proprietary & confidential to Analog Devices, Inc. and its licensors.
+*******************************************************************************/
+
+#ifndef TMC5271_SIMPLE_ROTATION_H_
+#define TMC5271_SIMPLE_ROTATION_H_
+
+#include "TMC5271.h"
+
+void initAllMotors(uint16_t icID);
+
+#endif
