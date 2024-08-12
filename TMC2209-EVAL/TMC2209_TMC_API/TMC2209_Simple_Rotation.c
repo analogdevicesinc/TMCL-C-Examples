8c423680ae903b15e5544bebdc9a320098b8a99f
Tushar Shiralkar <tushardinesh.shiralkar@analog.com>
2024-08-12 13:06:46 +0200

    Correct register configurations

--- TMC2209-EVAL/TMC2209_TMC_API/TMC2209_Simple_Rotation.c
+++ TMC2209-EVAL/TMC2209_TMC_API/TMC2209_Simple_Rotation.c
@@ -10,13 +10,12 @@
  * Configures the registers with the right settings that are needed for rotating the motor.
  * E.g Enabling driver, setting IRUN current etc.
  */
-void initAllMotors(uint16_t icID)
-{
-    tmc2209_writeRegister(icID, TMC2209_GCONF, 0x00000004);
-    tmc2209_writeRegister(icID, TMC2209_IHOLD_IRUN,0x00061F12);
-    // tmc2209_writeRegister(icID, TMC2209_TPOWERDOWN, 0x00000014);
-    tmc2209_writeRegister(icID, TMC2209_CHOPCONF, 0x10000053);      // Sets Irms=1.0A and Ihold=0.5A
-    // tmc2209_writeRegister(icID, TMC2209_PWMCONF, 0xC10D0024);
-
-    // Generate step pulse (S/D) externally via the connected microcontroller.
+void initAllMotors(uint16_t icID) {
+    tmc2209_writeRegister(icID, TMC2209_GCONF, 0x000000C0);         // GCONF
+    tmc2209_writeRegister(icID, TMC2209_SLAVECONF, 0x00000300);     // SLAVECONF (for UART address if needed)
+    tmc2209_writeRegister(icID, TMC2209_IHOLD_IRUN, 0x0000AF08);    // IHOLD_IRUN
+    tmc2209_writeRegister(icID, TMC2209_CHOPCONF, 0x10000053);      // CHOPCONF
+    tmc2209_writeRegister(icID, TMC2209_TPOWERDOWN, 0x00000014);
+    tmc2209_writeRegister(icID, TMC2209_PWMCONF, 0xC10D0024);
 }
+
