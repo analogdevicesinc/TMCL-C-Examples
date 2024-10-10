fc3cef24cd2bd1d69784dd9921fc04b1df95e746
Tushar Shiralkar <tushardinesh.shiralkar@analog.com>
2024-10-10 11:39:47 +0200

    Added esssential new api files

---
+++ TMC5072-EVAL/UART/TMC5072_Simple_Rotation.h
@@ -0,0 +1,14 @@
+/*******************************************************************************
+* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
+* This software is proprietary to Analog Devices, Inc. and its licensors.
+*******************************************************************************/
+
+
+#ifndef TMC5072_SIMPLE_ROTATION_H_
+#define TMC5072_SIMPLE_ROTATION_H_
+
+#include "TMC5072.h"
+
+void initAllMotors(uint16_t icID);
+
+#endif
