5014e403b5169e8d972b4e7ef0953f58484e7dc5
Aysha Sundas <Aysha.Sundas@analog.com>
2026-03-26 13:46:53 +0100

    Updated copyright text

--- TMC5130-EVAL/TMC5130_TMC_API/TMC5130_Simple_Rotation.h
+++ TMC5130-EVAL/TMC5130_TMC_API/TMC5130_Simple_Rotation.h
@@ -1,6 +1,5 @@
 /*******************************************************************************
-* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
-* This software is proprietary to Analog Devices, Inc. and its licensors.
+* Copyright © 2024 Analog Devices, Inc.
 *******************************************************************************/
 
 #ifndef TMC5130_SIMPLE_ROTATION_H_
--- TMC5271-EVAL/TMC5271_TMC_API/TMC5271_Simple_Rotation.h
+++ TMC5271-EVAL/TMC5271_TMC_API/TMC5271_Simple_Rotation.h
@@ -1,6 +1,5 @@
 /*******************************************************************************
-* Copyright © 2024 Analog Devices Inc. All Rights Reserved.
-* This software is proprietary to Analog Devices, Inc. and its licensors.
+* Copyright © 2024 Analog Devices, Inc.
 *******************************************************************************/
 
 #ifndef TMC5271_SIMPLE_ROTATION_H_
